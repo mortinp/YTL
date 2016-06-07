d20628d759d91359d427efd35dd183c3d4821d4d
unknown <mproenza@grm.desoft.cu>
2016-06-07 16:28:00 -0400

    yiki

---
+++ app/webroot/yiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+martin:$1$nNDUWnFa$kqAuyElda8JP6D8cTneow0:Martín:martin@yotellevocuba.com:admin,user
