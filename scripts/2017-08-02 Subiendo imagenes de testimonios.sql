6c418450dded319d0ebd8109b1a1a1010a4d9dfc
martin <mproenza@grm.desoft.cu>
2017-08-02 07:57:40 -0500

    subiendo imagenes de testimonios

---
+++ scripts/2017-08-02 Subiendo imagenes de testimonios.sql
@@ -0,0 +1,3 @@
+UPDATE testimonials SET image_filepath='files/1501678397_ddd_jpg' WHERE id='59814382-f5c0-44de-b3a6-58a44f8fb177';
+
+UPDATE testimonials SET image_filepath='files/1501678441_ooo_jpg' WHERE id='597e430d-33fc-4e2c-905d-19404f8fb177';
