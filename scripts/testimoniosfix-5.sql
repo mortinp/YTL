87e536d00fb849959c432698e78e3058a65604f4
martin <mproenza@grm.desoft.cu>
2018-04-03 09:27:06 -0500

    testimonios fix 5

---
+++ scripts/testimoniosfix-5.sql
@@ -0,0 +1,5 @@
+UPDATE testimonials SET image_filepath='files/1522765153_pedro_jpg' WHERE id='5abd18ef-3d40-46e3-9423-3d424f8fb177';
+
+UPDATE testimonials SET image_filepath='files/1522765215_roberto_jpg' WHERE id='5ab8066e-dfdc-40fb-ad5c-47234f8fb177';
+
+UPDATE testimonials SET image_filepath='files/1522765274_roberto2_jpg' WHERE id='5aa9362d-274c-4821-8b36-603b4f8fb177';
