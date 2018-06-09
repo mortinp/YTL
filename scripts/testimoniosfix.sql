4d63efd70ed021f2b9a51b9c685302867bb9f0ee
martin <mproenza@grm.desoft.cu>
2018-06-09 11:50:54 -0500

    test fix

--- scripts/testimoniosfix.sql
+++ scripts/testimoniosfix.sql
@@ -1,1 +1,1 @@
-UPDATE testimonials SET image_filepath='files/1525113306_ismel-kimara_jpg' WHERE id='5b1ad4e3-5b9c-45c3-9507-59cf4f8fb177';
+UPDATE testimonials SET image_filepath='files/1528562864_diego2ok_jpg' WHERE id='5b1ad4e3-5b9c-45c3-9507-59cf4f8fb177';
