02ff169f5dfef2011ee712933563c9034df11448
martin <mproenza@grm.desoft.cu>
2019-07-02 11:29:45 -0400

    arreglando core, email y database

--- app/Config/database.php
+++ app/Config/database.php
@@ -66,7 +66,7 @@ class DATABASE_CONFIG {
         'persistent' => false,
         'host' => 'localhost',
         'login' => 'root',
-        'password' => 'conficuba',
+        'password' => '',
         'database' => 'yotellevo',
         'prefix' => '',
         'encoding' => 'utf8',
