eaa95d1d795760a8b973beb54af6dcadf0796902
martin <mproenza@grm.desoft.cu>
2019-07-04 11:13:52 -0400

    cambiando scripts bd tarea 8

---
+++ scripts/2019-06-28 Added table testimonials_replies.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `testimonials_replies` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `testimonial_id` CHAR(36) NOT NULL ,
+  `reply_text` TEXT NOT NULL ,
+  `reply_by` SET('driver','traveler') NOT NULL ,
+  `state` SET('P','A','R') NOT NULL DEFAULT 'P' ,
+  `created` DATETIME NOT NULL ,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+ ) ENGINE = InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
