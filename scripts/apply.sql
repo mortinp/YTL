f2d19042cce73e62e97db78105a8d3734db629aa
martin <mproenza@grm.desoft.cu>
2019-07-08 00:02:35 -0400

    apply.sql

--- scripts/apply.sql
+++ scripts/apply.sql
@@ -1,1 +1,11 @@
-ALTER TABLE  `api_sync_queue_2driver_conversations` ADD  `batch_id_retry_count` INT NOT NULL DEFAULT  '0';
+CREATE TABLE IF NOT EXISTS `testimonials_replies` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `testimonial_id` CHAR(36) NOT NULL ,
+  `reply_text` TEXT NOT NULL ,
+  `reply_by` SET('driver','traveler') NOT NULL ,
+  `state` SET('P','A','R') NOT NULL DEFAULT 'P' ,
+  `created` DATETIME NOT NULL ,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+ ) ENGINE = InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+ALTER TABLE `testimonials` ADD `driver_reply_token` VARCHAR(32) NULL DEFAULT NULL AFTER `validation_token`; 
