0e82f4da9ec88e05925a8a7dd65d468fc5a9c23d
martin <mproenza@grm.desoft.cu>
2019-07-11 16:16:58 -0400

    apply

--- scripts/apply.sql
+++ scripts/apply.sql
@@ -1,5 +1,7 @@
-ALTER TABLE  `driver_traveler_conversations` CHANGE  `conversation_id`  `conversation_id` CHAR( 36 ) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL;
+ALTER TABLE  `api_sync_queue_2driver_conversations` CHANGE  `conversation_id`  `conversation_id` CHAR( 36 ) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL;
 
-ALTER TABLE  `driver_traveler_conversations` ADD FOREIGN KEY (  `conversation_id` ) REFERENCES  `yotellevo`.`drivers_travels` (
+ALTER TABLE  `api_sync_queue_2driver_conversations` ADD INDEX (  `conversation_id` );
+
+ALTER TABLE  `api_sync_queue_2driver_conversations` ADD FOREIGN KEY (  `conversation_id` ) REFERENCES  `yotellevo`.`drivers_travels` (
 `id`
 ) ON DELETE RESTRICT ON UPDATE RESTRICT ;
