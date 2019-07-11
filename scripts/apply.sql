ff058f3eb72d07a37f2f7399f60062a0f97b3cd8
martin <mproenza@grm.desoft.cu>
2019-07-11 16:02:18 -0400

    forgot apply.sql :)

--- scripts/apply.sql
+++ scripts/apply.sql
@@ -1,2 +1,23 @@
-ALTER TABLE  `drivers` ADD  `email_active` BOOLEAN NOT NULL DEFAULT  '1';
-ALTER TABLE  `email_attachments` CHANGE  `email_queue_id`  `email_queue_id` CHAR( 36 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;
+ALTER TABLE  `driver_traveler_conversations` CHANGE  `conversation_id`  `conversation_id` CHAR( 36 ) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL;
+
+ALTER TABLE  `driver_traveler_conversations` ADD FOREIGN KEY (  `conversation_id` ) REFERENCES  `yotellevo`.`drivers_travels` (
+`id`
+) ON DELETE RESTRICT ON UPDATE RESTRICT ;
+
+
+
+ALTER TABLE  `api_sync_queue_2driver_conversations` CHANGE  `conversation_id`  `conversation_id` CHAR( 36 ) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL;
+
+ALTER TABLE  `api_sync_queue_2driver_conversations` ADD INDEX (  `conversation_id` );
+
+ALTER TABLE  `api_sync_queue_2driver_conversations` ADD FOREIGN KEY (  `conversation_id` ) REFERENCES  `yotellevo`.`drivers_travels` (
+`id`
+) ON DELETE RESTRICT ON UPDATE RESTRICT ;
+
+
+
+ALTER TABLE  `driver_traveler_conversations` CHANGE  `id`  `id` BIGINT( 20 ) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE  `api_sync_queue_2driver_conversations` ADD FOREIGN KEY (  `msg_id` ) REFERENCES  `yotellevo`.`driver_traveler_conversations` (
+`id`
+) ON DELETE RESTRICT ON UPDATE RESTRICT ;
