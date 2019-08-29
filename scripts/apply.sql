cf78ada432655c09fcfa8836be6e5d63fd854900
martin <mproenza@grm.desoft.cu>
2019-08-29 16:05:23 -0400

    apply

--- scripts/apply.sql
+++ scripts/apply.sql
@@ -1,2 +1,1 @@
-ALTER TABLE  `drivers` ADD  `has_classic_car` BOOLEAN NOT NULL DEFAULT  '0' AFTER  `has_modern_car`;
-ALTER TABLE `testimonials` ADD `use_as_sample` BOOLEAN NOT NULL DEFAULT  '0' AFTER `featured`;
+ALTER TABLE `users` ADD `pass_was_created_by_user` BOOLEAN NOT NULL DEFAULT FALSE AFTER `shared_ride_offered`;
